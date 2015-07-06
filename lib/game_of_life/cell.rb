85467d97f8c5ab1103161ce116762a2d19843c60
spraveen1112 <spraveen1112@gmail.com>
2015-07-06 11:42:43 +0530

    add cell class and modifiaction in cell_spec.

---
+++ lib/game_of_life/cell.rb
@@ -0,0 +1,11 @@
+class Cell
+  ALIVE = "alive"	
+  def initialize status
+    @status = status  
+  end	
+
+  def state 
+  	 @status	
+  end	
+
+end
--- spec/game_of_life/cell_spec.rb
+++ spec/game_of_life/cell_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Cell  do
   it "New cell should be alive" do 
   	ALIVE="alive"
-	cell = Cell.new  	    	
-	expect(cell.status).to eq(ALIVE) 
+	cell = Cell.new ALIVE 	    	
+	expect(cell.state).to eq(ALIVE) 
   end
 end  
